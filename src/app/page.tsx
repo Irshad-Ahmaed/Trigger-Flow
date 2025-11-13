2b5b3bb4cf717fb333d4f204ff18ace16bd42a86
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-11-13 13:48:20 +0530

    6th Commit: Background jobs initialize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-import { requireAuth } from "@/lib/auth-utils";
+'use client';
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-const page = async () => {
-  await requireAuth();
+const page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  const { data } = useQuery(trpc.getWorkflows.queryOptions())
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: ()=> {
+      queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
+    }
+  }));
+
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      Protected server component
+    <div className="min-h-screen min-w-screen flex items-center flex-col justify-center">
+      Protected server component 
+      <p>
+        {JSON.stringify(data, null, 2)}
+      </p>
+
+      <Button disabled={create.isPending} onClick={()=> create.mutate()}>
+        Create Workflow
+      </Button>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,25 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
+import prisma from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure
-    .query(({ ctx }) => {
-      // console.log({userId: ctx.auth.user.id});
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workFlow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async()=> {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: 'irsamd07@gmail.com'
+      },
+    });
 
-      return prisma.user.findMany({
-        where: {
-          id: ctx.auth.user.id,
-        },
-      });
-    }),
+    return prisma.workFlow.create({
+      data: {
+        name: "test-workflow"
+      }
+    })
+  })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
