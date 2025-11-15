c8807ca269f9505bacbd8a396f37296c2e5c6f2f
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-11-15 11:11:15 +0530

    8th commit: error tracking by sentry

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,8 +1,9 @@
 import { inngest } from "./client";
+import * as Sentry from "@sentry/nextjs";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
-import { createOpenAI } from '@ai-sdk/openai';
-import { createGroq  } from '@ai-sdk/groq';
+import { createOpenAI } from "@ai-sdk/openai";
+import { createGroq } from "@ai-sdk/groq";
 
 const google = createGoogleGenerativeAI();
 const openai = createOpenAI();
@@ -12,29 +13,62 @@ export const executeAI = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
   async ({ event, step }) => {
-    const { steps: geminiSteps } = await step.ai.wrap("gemini-generate-text", generateText, {
-      model: google('gemini-2.5-flash'),
-      system: 'You are a helpful assistant.',
-      prompt: 'What is 2+2 ?'
-    });
-    
-    const { steps: groqSteps } = await step.ai.wrap("groq-generate-text", generateText, {
-      model: groq('openai/gpt-oss-120b'),
-      system: 'You are a helpful assistant.',
-      prompt: 'What is 2+2 ?'
-    });
 
-    const { steps: openaiSteps } = await step.ai.wrap("openai-generate-text", generateText, {
-      model: openai('gpt-4'),
-      system: 'You are a helpful assistant.',
-      prompt: 'What is 2+2 ?'
-    });
-    
+    await step.sleep('pretend', '5s');
+
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
+    console.warn("Error might detect by sentry");
+    console.log('Error occur');
+
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2+2 ?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
+
+    const { steps: groqSteps } = await step.ai.wrap(
+      "groq-generate-text",
+      generateText,
+      {
+        model: groq("openai/gpt-oss-20b"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2+2 ?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
+
+    const { steps: openaiSteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-4"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2+2 ?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      }
+    );
 
     return {
       geminiSteps,
       openaiSteps,
-      groqSteps
+      groqSteps,
     };
-  },
+  }
 );
